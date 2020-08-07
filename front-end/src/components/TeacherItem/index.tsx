0b10a7e8ca0377f906890ba98bde797929f21a88
Izaias neto <izaias_neto_@live.com>
2020-08-07 17:53:46 -0300

    projeto finalizado!

--- front-end/src/components/TeacherItem/index.tsx
+++ front-end/src/components/TeacherItem/index.tsx
@@ -3,33 +3,50 @@ import React from 'react'
 import whatsappicon from '../../assets/images/icons/whatsapp.svg'
 
 import './style.css'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+    
+        id: number;
+        avatar: string;
+        bio: string;
+        cost: number;
+        name: string;
+        subject: string;
+        whatsapp: string;
+    
+}
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
     return (
         <article className="teacher-item">
         <header>
-            <img src="https://pbs.twimg.com/profile_images/1278439428101472257/Hnu-UOZu_400x400.jpg" alt="Izaias Neto"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-                <strong>Izaias Neto</strong>
-                <span>Quimica</span>
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
             </div>
         </header>
 
-        <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br/><br/>
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-        </p>
+    <p>{teacher.bio}</p>
 
         <footer>
             <p>
                 Preço/hora
-                <strong>R$ 80,00</strong>
+                <strong>R$ {teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a target="_blank" onClick={createNewConnection}href={`https://wa.me/${teacher.whatsapp}`}>
                 <img src={whatsappicon} alt="whatsapp"/>
                 Entrar em contato
-            </button>
+            </a>
             
         </footer>
     </article>
