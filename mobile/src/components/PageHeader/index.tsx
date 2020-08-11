0967244986d34c099260f3f6872ff3a77e4f24d8
Izaias neto <izaias_neto_@live.com>
2020-08-11 16:42:01 -0300

    mobile

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,43 @@
+import React, {ReactNode}from 'react'
+import { View, Image, Text } from 'react-native'
+import { BorderlessButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+
+import backicon from '../../assets/images/icons/back.png'
+import logoImg from '../../assets/images/logo.png'
+
+import styles from './styles'
+
+interface PageheaderProps{
+    title: string;
+    headerRight?: ReactNode;
+}
+const PageHeader:React.FC<PageheaderProps> = ({title, headerRight, children}) =>{
+    const { navigate } = useNavigation()
+    function handlerGoBack(){
+        navigate('Landing')
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.topBar}>
+                <BorderlessButton
+                    onPress={handlerGoBack}
+                >
+                    <Image source={backicon} resizeMode="contain"/>
+                </BorderlessButton>
+
+                <Image source={logoImg} resizeMode="contain"/>
+            </View>
+            
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+                {headerRight}
+            </View>
+            
+            {children}
+
+        </View>
+    )
+}
+
+export default PageHeader
